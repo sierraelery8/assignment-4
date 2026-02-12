1ea490af813f09b416d46e3e5424315b983b29ff
sierraelery8 <sierra.elery@centenaryuniversity.edu>
2026-02-11 21:29:05 -0500

    server.js

--- server.js
+++ server.js
@@ -96,6 +96,10 @@ app.delete('/api/books/:id', (req, res) => {
 
 // Start server
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
