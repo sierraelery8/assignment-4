b483c5ccfc6e7da204061d7d38b4e19e5b2be8f0
sierraelery8 <sierra.elery@centenaryuniversity.edu>
2026-02-11 21:04:47 -0500

    server.js

--- server.js
+++ server.js
@@ -99,10 +99,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-// Start server
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
